84cdeb66f32a1af6a9ab920e1fd1de9f105ace80
Hatthawat400 <commy2812@gmail.com>
2020-08-21 22:47:59 +0700

    love

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <div>
+      <slide />
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <img src="./assets/22.jpg" alt />
+        <counter @cc="sum" />
+      </div>
+      <div class="col-md-6 mb-3 a">
+        <h1>Totol = {{total}}</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <img src="./assets/33.jpg" alt />
+        <counter @bb="sumb" />
+      </div>
+      <div class="col-md-6 mb-3 a">
+        <h1>Totol = {{tot}}</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <img src="./assets/44.jpg" alt />
+        <counter @dd="sumc" />
+      </div>
+      <div class="col-md-6 mb-3 a">
+        <h1>Totol = {{tota}}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter";
+import slide from "./components/slide";
+export default {
+  name: "App",
+  components: {
+    Counter,
+    slide,
+  },
+  data: function () {
+    return {
+      total: 0, //ผลรวมทั้งหมด ---->เอาไปแสดงผล {{}}
+      tot: 0,
+      tota:0,
+    };
+  },
+
+  methods: {
+    sum(CounterValue) {
+      this.total += CounterValue;
+    },
+    sumb(Value) {
+      this.tot += Value;
+    },
+    sumc(Va) {
+      this.tota += Va;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.a {
+  height: 300px;
+  margin: auto;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/slide.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img :src="img[0]" /></div>
+        <div class="carousel-item">
+          <img :src="img[1]" /></div>
+        <div class="carousel-item">
+          <img :src="img[2]" /></div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      img: [
+        require("../assets/3.jpg"),
+        require("../assets/4.jpg"),
+        require("../assets/5.jpg"),
+      ],
+    };
+  },
+};
+</script>
+
+
+
+<style>
+</style>
